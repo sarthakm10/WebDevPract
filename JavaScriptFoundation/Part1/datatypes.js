b44c2960e55f2d19a34d9d6a6bd88ddea570e818
sarthakm10 <mishrasarthak33@gmail.com>
2024-12-16 13:59:19 +0530

    added some js foundation files

---
+++ JavaScriptFoundation/Part1/datatypes.js
@@ -0,0 +1,34 @@
+// Types of Data types
+/*
+String
+Number
+Boolean
+Bigint
+
+Undefined
+null
+
+object
+symbol
+*/
+
+// var score = 102;
+let score = 103;
+let name = "Sarthak";
+let isLoggedin = false;
+
+// Object
+let fruitTypes = ["Apple", "Banana", "Oranges"];
+let user = {firstName: "sarthak", lastName: "Mishra"};
+
+let getScore = score;
+
+console.log(getScore);
+console.log(name);
+console.log(isLoggedin);
+console.log(fruitTypes);
+console.log(user);
+
+
+
+
---
+++ JavaScriptFoundation/Part1/printing.js
@@ -0,0 +1,11 @@
+console.log("Hello");
+// This is another way of printing in console but it doesn't create a new line
+process.stdout.write("chai");
+process.stdout.write("\n");
+
+// Some other ways of printing in console
+console.table({city: "Mumbai"});
+// This prints in some color in console
+console.warn({city: "Delhi"});
+
+
