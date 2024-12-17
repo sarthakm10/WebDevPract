98f3d3eb551633bb700bef476a580fb97173423e
sarthakm10 <mishrasarthak33@gmail.com>
2024-12-17 22:02:51 +0530

    added js files

---
+++ JavaScriptFoundation/Part1/operations.js
@@ -0,0 +1,42 @@
+let score = 102;
+let bonus = 25;
+
+let totalScore = score + bonus;
+console.log(totalScore);
+
+let addition = 4 + 5;
+console.log(addition);
+
+let subtract = 9 - 3;
+console.log(subtract);
+
+let mult = 3 * 5;
+console.log(mult);
+
+let divi = 8 / 2;
+console.log(divi);
+
+let reminder = 9 % 2;
+console.log(reminder);
+
+let expo = 2 ** 3;
+console.log(expo);
+
+let myscore = 110;
+myscore++;
+console.log(myscore);
+
+let credits = 56;
+credits--;
+console.log(credits);
+
+// Comparion operation
+
+let num1 = 3;
+let num2 = 3;
+let num3 = 6;
+
+console.log(num1 == num2); //true
+console.log(num1 != num3); // true
+console.log(num1 > num3); // true
+console.log(num1 < num3); // true
